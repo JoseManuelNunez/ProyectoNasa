34917a5613a178f9cf8abe1457f47004851e904f
JoseManuelNunez <josemanuelnunezreyes@gamil.com>
2023-11-25 19:44:14 -0400

    setup application

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>api de la nasa de jose manuel y joel</Text>
       <StatusBar style="auto" />
     </View>
   );
