ae8ae6f181b21d6585af82bda114286073f7671e
JoseManuelNunez <josemanuelnunezreyes@gamil.com>
2023-11-25 23:16:01 -0400

    create lastFiveDays component

---
+++ src/components/molecules/LastFiveDaysImages.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import TextAtom from "../atoms/TextAtom";
+import ButtonAtom from "../atoms/ButtonAtom";
+
+export default function LastFiveDaysImages({ LastFiveDaysImages }) {
+
+    return (
+        <View style={styles.container}>
+            <TextAtom text="Last 5 Days" style={styles.title} />
+            <ScrollView style={styles.content}>
+                {LastFiveDaysImages &&
+                    LastFiveDaysImages.map((imageData, index) => (
+                        <View style={styles.dataContent} key={index}>
+                            <TextAtom text={imageData.title} style={styles.dataTitle} />
+                            <TextAtom text={imageData.date} style={styles.dataDate} />
+                            <ButtonAtom
+                                title="View"
+                                style={styles.button}
+                            />
+                        </View>
+                    ))}
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginVertical: 8,
+    },
+    content: {
+        paddingHorizontal: 20,
+    },
+    title: {
+        color: "#fff",
+        fontSize: 15,
+        marginBottom: 18,
+        textAlign: "center",
+    },
+    dataContent: {
+        marginTop: 16,
+        backgroundColor: "#2c449d",
+        borderRadius: 32,
+        padding: 16,
+        marginHorizontal: 24,
+    },
+    dataTitle: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+        marginVertical: 12,
+    },
+    dataDate: {
+        color: "#bbb",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    button: {
+        alignItems: "flex-end",
+    },
+});
