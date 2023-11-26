5256f518a67377b4aa2bed44a4c5b682d86d5a67
JoseManuelNunez <josemanuelnunezreyes@gamil.com>
2023-11-25 23:04:37 -0400

    create todaysImage component

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,10 @@ const API_KEY = process.env.API_KEY
 const API_URL = process.env.API_URL
 
 const fetchNASAData = async (urlParams = "") => {
-    const apiUrlWithParams = `${API_URL}?api_key=${API_KEY}${urlParams}`;
+    const apiUrlWithParams = `${'https://api.nasa.gov/planetary/apod'}?api_key=${'QW4HDTmy6iQEb5igmCuYsYyVw9wIXxaiXtw8ihsB'}${urlParams}`;
     try {
         const response = await fetch(apiUrlWithParams);
+        console.log(response)
         const data = await response.json();
         return data;
     } catch (error) {
